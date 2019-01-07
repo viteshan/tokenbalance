f0f0a51ae7a3d2c18dc1d1ee5ef02dbf541348d4
viteshan <viteshan@163.com>
2019-01-07 12:41:44 +0800

    add token transfer test

---
+++ tokentransfer_test.go
@@ -0,0 +1,109 @@
+package tokenbalance
+
+import (
+	"context"
+	"fmt"
+
+	"math/big"
+	"strings"
+	"testing"
+
+	ethereum "github.com/ethereum/go-ethereum"
+	"github.com/ethereum/go-ethereum/accounts/abi"
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/ethereum/go-ethereum/crypto"
+	"github.com/ethereum/go-ethereum/ethclient"
+	"github.com/vitelabs/go-vite/log15"
+)
+
+// LogTransfer ..
+type LogTransfer struct {
+	From  common.Address
+	To    common.Address
+	Value *big.Int
+}
+
+func TestClient(t *testing.T) {
+	log15.Root().SetHandler(
+		log15.LvlFilterHandler(log15.LvlInfo, log15.Must.FileHandler("/Users/jie/log/transfers.log", log15.JsonFormat())),
+	)
+	//client, err := ethclient.Dial("http://192.168.31.49:8545")
+	client, err := ethclient.Dial("http://148.70.13.221:8545")
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	const tokenABI = "[{\"constant\":true,\"inputs\":[],\"name\":\"mintingFinished\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"name\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_spender\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"approve\",\"outputs\":[],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"totalSupply\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_from\",\"type\":\"address\"},{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transferFrom\",\"outputs\":[],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"decimals\",\"outputs\":[{\"name\":\"\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"unpause\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_amount\",\"type\":\"uint256\"}],\"name\":\"mint\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"paused\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"}],\"name\":\"balanceOf\",\"outputs\":[{\"name\":\"balance\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"finishMinting\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[],\"name\":\"pause\",\"outputs\":[{\"name\":\"\",\"type\":\"bool\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"owner\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[],\"name\":\"symbol\",\"outputs\":[{\"name\":\"\",\"type\":\"string\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_value\",\"type\":\"uint256\"}],\"name\":\"transfer\",\"outputs\":[],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"_to\",\"type\":\"address\"},{\"name\":\"_amount\",\"type\":\"uint256\"},{\"name\":\"_releaseTime\",\"type\":\"uint256\"}],\"name\":\"mintTimelocked\",\"outputs\":[{\"name\":\"\",\"type\":\"address\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":true,\"inputs\":[{\"name\":\"_owner\",\"type\":\"address\"},{\"name\":\"_spender\",\"type\":\"address\"}],\"name\":\"allowance\",\"outputs\":[{\"name\":\"remaining\",\"type\":\"uint256\"}],\"payable\":false,\"type\":\"function\"},{\"constant\":false,\"inputs\":[{\"name\":\"newOwner\",\"type\":\"address\"}],\"name\":\"transferOwnership\",\"outputs\":[],\"payable\":false,\"type\":\"function\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Mint\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[],\"name\":\"MintFinished\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[],\"name\":\"Pause\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[],\"name\":\"Unpause\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"owner\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"spender\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Approval\",\"type\":\"event\"},{\"anonymous\":false,\"inputs\":[{\"indexed\":true,\"name\":\"from\",\"type\":\"address\"},{\"indexed\":true,\"name\":\"to\",\"type\":\"address\"},{\"indexed\":false,\"name\":\"value\",\"type\":\"uint256\"}],\"name\":\"Transfer\",\"type\":\"event\"}]"
+	contractAbi, err := abi.JSON(strings.NewReader(tokenABI))
+	if err != nil {
+		t.Fatal(err)
+	}
+
+	// 5491020,7014140
+	contractAddress := common.HexToAddress("0x1b793e49237758dbd8b752afc9eb4b329d5da016")
+
+	//index := int64(5750214)
+	index := int64(7005494)
+	step := int64(200)
+	max := int64(7014140)
+
+	//queryPrint(client, big.NewInt(6758098), big.NewInt(6758098), contractAddress)
+	for {
+		end := index + step
+		if end > max {
+			end = max
+		}
+		startBig := big.NewInt(index)
+		endBig := big.NewInt(end)
+		err := queryPrint(client, startBig, endBig, contractAddress, contractAbi)
+		if err != nil {
+			t.Fatal(err)
+		}
+		if end >= max {
+			break
+		}
+		index = end
+	}
+
+}
+
+func queryPrint(cli *ethclient.Client, start, end *big.Int, contractAddress common.Address, abi2 abi.ABI) error {
+	query := ethereum.FilterQuery{
+		FromBlock: start,
+		ToBlock:   end,
+		Addresses: []common.Address{
+			contractAddress,
+		},
+	}
+
+	logs, err := cli.FilterLogs(context.Background(), query)
+
+	if err != nil {
+		return err
+	}
+
+	logTransferSig := []byte("Transfer(address,address,uint256)")
+	logTransferSigHash := crypto.Keccak256Hash(logTransferSig)
+
+	//lastNumber := uint64(0)
+	for _, vLog := range logs {
+		switch vLog.Topics[0].Hex() {
+		case logTransferSigHash.Hex():
+			var transferEvent LogTransfer
+			err := abi2.Unpack(&transferEvent, "Transfer", vLog.Data)
+			if err != nil {
+				panic(err)
+			}
+
+			transferEvent.From = common.HexToAddress(vLog.Topics[1].Hex())
+			transferEvent.To = common.HexToAddress(vLog.Topics[2].Hex())
+
+			//lastNumber = vLog.BlockNumber
+			//log15.Info(fmt.Sprintf("%d,%s,%s,%s,%s", vLog.BlockNumber, vLog.BlockHash.Hex(), transferEvent.From.Hex(), transferEvent.To.Hex(), transferEvent.Value.String()))
+			fmt.Println(fmt.Sprintf("%d,%s,%s,%s,%s", vLog.BlockNumber, vLog.BlockHash.Hex(), transferEvent.From.Hex(), transferEvent.To.Hex(), transferEvent.Value.String()))
+		}
+	}
+
+	//fmt.Println("Number", strconv.FormatUint(lastNumber, 10), time.Now().String())
+	return nil
+}
